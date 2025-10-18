2db160040b1da16a72f3513bb5c843741258a25c
Кудиненко Кирило <k.o.kudynenko@student.khai.edu>
2025-10-18 14:48:50 +0300

    ChatBot 1-st stage
    ChatBot 2-nd stage
    
    ChatBot 2-rd stage
    
    ChatBot 4-th stage
    
    ChatBot 5-th stage

---
+++ ChatBot/ChatBot.py
@@ -0,0 +1,34 @@
+print("Hello! My name is First_Bot.")
+print("I was created in 2025.")
+
+name = input("Please, remind me your name:\n>>> ")
+print("What a great name you have, " + name + "!")
+
+print("Let me guess your age.")
+print("Enter remainders of dividing your age by 3, 5 and 7.")
+remainder3 = int(input(">>> "))
+remainder5 = int(input(">>> "))
+remainder7 = int(input(">>> "))
+
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+count_number = int(input("Now I will prove to you that I can count to any number you want.\n>>> "))
+for i in range(count_number + 1):
+    print(f"{i} !")
+
+print("Let's test your programming knowledge.")
+print("Why do we use methods?")
+print("1. To repeat a statement multiple times.")
+print("2. To decompose a program into several small subroutines.")
+print("3. To determine the execution time of a program.")
+print("4. To interrupt the execution of a program.")
+
+while True:
+    user_answer = input(">>> ")
+    if user_answer == "2":
+        print("Completed, have a nice day!")
+        print("Congratulations, have a nice day!")
+        break
+    else:
+        print("Please, try again.")
